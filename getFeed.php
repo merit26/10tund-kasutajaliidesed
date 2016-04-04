6cd8c1b57a6b604606f3c2d62cb5b1d147e6e7ff
Merit Paist <meripai@greeny.cs.tlu.ee>
2016-04-04 12:27:48 +0300

    new files

---
+++ getFeed.php
@@ -0,0 +1,8 @@
+<<?php
+//getFeed.php?search=%23Tallinn # on %23
+//https://raw.githubusercontent.com/J7mbo/twitter-api-php/master/TwitterAPIExchange.php
+require_once("twitterAPIExcahnge.php");
+require_once("config.php");
+
+
+ ?>
